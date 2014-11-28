08f19e9ac4c8bef5b457da53bfe0a5494ffc9a4f
Robert Lowe <robertmlowe@rmlowe.com>
2014-11-28 19:06:21 +0800

    Code

--- List Files/Program.cs
+++ List Files/Program.cs
@@ -10,6 +10,13 @@ namespace List_Files
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("This program lists all the files in the directory.");
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(@"C:\");
+            foreach (System.IO.FileInfo file in dir.GetFiles("*.*"))
+            {
+                Console.WriteLine("{0}, {1}", file.Name, file.Length);
+            }
+            Console.ReadLine();
         }
     }
 }
